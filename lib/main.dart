0c64aa37394d67881346278982383bcce2ae17f1
brr <2274252@cegepmontpetit.ca>
2024-12-10 10:56:30 -0500

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tp1/acceuil.dart';
 import 'package:tp1/connexion.dart';
 import 'package:tp1/consultation.dart';
 import 'package:tp1/creation.dart';
+import 'package:tp1/firebase_options.dart';
 import 'package:tp1/inscription.dart';
 import 'generated/l10n.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
